372f24193a8b287d4e455b7df112d3701e8612e5
Siddardh2987 <venkatasiddhu2987@gmail.com>
2025-12-20 17:23:37 +0530

    AuthController Added

---
+++ Server/controllers/authcontrollers.js
@@ -0,0 +1,76 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import userModel from '../models/userModel.js';
+
+export const register = async (req , res) => {
+    const {name , email , password} = req.body;
+    if(!name || !email || !password){
+        return res.json({success : false , message : "Missing Details."})
+    }
+    try{
+        const existingUser = await userModel.findOne({email});
+        if(existingUser){
+            return res.json({success : false , message : "User already exists."})
+        }
+        const hashedPassword = await bcrypt.hash(password , 10);
+        const user = new userModel({
+            name,
+            email,
+            password : hashedPassword
+        });
+        await user.save();
+
+        const token = jwt.sign({userId : user._id} , process.env.JWT_SECRET , {expiresIn : '1d'});
+        res.cookie('token' , token , {
+            httpOnly : true,
+            secure : process.env.NODE_ENV === 'production',
+            sameSite : process.env.NODE_ENV === 'production'? 'none' : 'strict',
+            maxAge : 24 * 60 * 60 * 1000 // 1day in ms , our jwt token expiry is 1 day
+        });
+        return res.json({success : true, message : "User registered successfully."})
+    }catch(error){
+        return res.json({success : false, message : error.message})
+    }
+}
+
+export const login = async (req , res) => {
+    const {email , password} = req.body;
+    if(!email || !password){
+        return res.json({success : false , message : "Missing Details."})
+    }
+    try{
+        const existingUser = await userModel.findOne({email});
+        if(!existingUser){
+            return res.json({success : false , message : "User doesn't exists."})
+        }
+        const isMatch = await bcrypt.compare(password , existingUser.password);
+        if(!isMatch){
+            return res.json({success : false , message : "Invalid Credentials."})
+        }
+        const token = jwt.sign({userId : existingUser._id} , process.env.JWT_SECRET , {expiresIn : '1d'});
+        res.cookie('token' , token , {
+            httpOnly : true,
+            secure : process.env.NODE_ENV === 'production',
+            sameSite : process.env.NODE_ENV === 'production'? 'none' : 'strict',
+            maxAge : 24 * 60 * 60 * 1000 // 1day in ms , our jwt token expiry is 1 day
+        });
+        return res.json({success : true, message : "User logged in successfully."})
+    }catch(error){
+        return res.json({success : false, message : error.message})
+    }
+}
+
+export const logout = async (req,res) => {
+    try{
+        res.clearCookie('token' , {
+            httpOnly : true,
+            secure : process.env.NODE_ENV === 'production',
+            sameSite : process.env.NODE_ENV === 'production'? 'none' : 'strict',
+        });
+
+        return res.json({success : true, message : "User logged out successfully."});
+
+    }catch(error){
+        return res.json({success : false, message : error.message});
+    }
+}
